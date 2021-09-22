81f68218e448e82d6f8376b95f9ee73713a056c9
andrei.tuguiu@totalsoft.ro <andrei.tuguiu@totalsoft.ro>
2021-09-22 12:18:09 +0300

    welcome commit

--- src/constants/menuConfig.js
+++ src/constants/menuConfig.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import Dashboard from '@material-ui/icons/Dashboard'
+import Welcome from '@material-ui/icons/Home'
 import Settings from '@material-ui/icons/Settings'
 import HomeIcon from "@material-ui/icons/Home"
 
 const menuItems = [
-  { icon: <Dashboard />, text: 'NavBar.Dashboard', path: '/dashboard', name: 'Dashboard' },
+  { icon: <Welcome />, text: 'NavBar.Welcome', path: '/welcome', name: 'Welcome' },
   { icon: <Settings />, text: 'NavBar.Settings', path: '/settings', name: 'Settings' },
   { icon: <HomeIcon />, text: 'NavBar.MyFirstMenu', path: '/helloWorld', name: 'MyFirstMenu' }
 ]
---
+++ src/features/welcome/Welcome.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Typography, Grid } from '@material-ui/core'
+import { useTranslation } from 'react-i18next'
+import CustomTextField from '@bit/totalsoft_oss.react-mui.custom-text-field'
+import CustomIconButton from '@bit/totalsoft_oss.react-mui.icon-button'
+import KeyboardReturnIcon from '@material-ui/icons/KeyboardReturn';
+
+
+function Welcome() {
+  const { t } =useTranslation()
+  return (
+      <Grid container justify="center" alignItems="center" alignContent="center" direction="column" spacing={10}>
+        <Grid item xs={4}>
+          <Typography variant="h5">{t("LandingPage.Title")}</Typography>
+        </Grid>
+        <Grid item container justify="center" alignContent="center" direction="column" spacing={1}>
+          <Grid item xs={12}>
+            <Typography variant="caption">{t("LandingPage.Subtitle")}</Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <CustomTextField
+              fullWidth
+              endAdornment={
+                <CustomIconButton size="small" color="theme" aria-label="go">
+                  <KeyboardReturnIcon fontSize="small" />
+                </CustomIconButton>
+              }
+              />
+          </Grid>
+        </Grid>
+       </Grid>
+  )
+}
+
+export default Welcome
