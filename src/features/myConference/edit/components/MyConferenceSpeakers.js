cef842f80d6fdeb482ddb2894fdabe27635ba42c
andrei.tuguiu@totalsoft.ro <andrei.tuguiu@totalsoft.ro>
2021-09-24 17:28:24 +0300

    speakers

--- src/features/myConference/edit/components/MyConferenceSpeakers.js
+++ src/features/myConference/edit/components/MyConferenceSpeakers.js
@@ -1,7 +1,45 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import tableStyles from 'assets/jss/components/tableStyle'
+import { Grid, makeStyles } from '@material-ui/core'
+import {Table, Thead,Tbody, Tr, Th} from 'react-super-responsive-table'
+import { useTranslation } from 'react-i18next'
+import MyConferenceSpeakerData from './MyConferenceSpeakerData'
 
-const MyConferenceSpeakers = () => {
-  return <>Hello World!</>
+const useStyles = makeStyles(tableStyles)
+
+const MyConferenceSpeakers = props => {
+  const { speakers } = props
+  const {t}=useTranslation()
+  const classes = useStyles()
+  return (
+    <Grid className={classes.enableScrollX}>
+      <Table className={classes.table}>
+          <Thead>
+              <Tr>
+                  <Th className={classes.tableHeader}>{t('Speaker.Name')}</Th>
+                  <Th className={classes.tableHeader}>{t('Speaker.Nationality')}</Th>
+                  <Th className={classes.tableHeader}>{t('Speaker.Rating')}</Th>
+                  <Th className={classes.tableHeader}>{t('Speaker.MainSpeaker')}</Th>
+                  <Th className={classes.tableHeader}></Th>
+              </Tr>
+          </Thead>
+          <Tbody>
+              {speakers?.map((speaker)=>(
+                  <MyConferenceSpeakerData key={speaker?.id} speaker={speaker} />
+              ))}
+          </Tbody>
+      </Table>
+    </Grid>
+  )
+}
+
+MyConferenceSpeakers.propTypes = {
+  speakers: PropTypes.array
+}
+
+MyConferenceSpeakers.defaultProps = {
+  speakers: [{}]
 }
 
 export default MyConferenceSpeakers
